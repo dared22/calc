55004458ae5251b0c6b4f12f1a2fbc7861115f34
dared22 <dared22@yandex.ru>
2024-07-30 18:23:05 +0200

    changed site layout

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def send_email():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5500))
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
